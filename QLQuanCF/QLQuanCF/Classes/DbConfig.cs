3e3d3c8db24084d5b82365d244268a861d54afcf
justwannacodee <98950227+justwannacodee@users.noreply.github.com>
2024-11-05 14:44:59 +0700

    add CaLamViec, ChiTietLuong, HDB, HDN models

--- QLQuanCF/QLQuanCF/Classes/DbConfig.cs
+++ QLQuanCF/QLQuanCF/Classes/DbConfig.cs
@@ -3,6 +3,7 @@ namespace QLQuanCF.Classes
 {
     public static class DbConfig
     {
-        public static string connectString = "Server=LAPTOP-CDVPPHBM;Database=QLQuanCF;User ID=sa;Password=abc123;";
+        //public static string connectString = "Server=LAPTOP-CDVPPHBM;Database=QLQuanCF;User ID=sa;Password=abc123;";
+        public static string connectString = "Server=LAPTOP-3JRHUEFF\\SQLEXPRESS01;Database=QLQuanCF;User ID=sa;Password=minhquan2004;";
     }
 }
---
+++ QLQuanCF/QLQuanCF/Models/HoaDonBan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLQuanCF.Models
+{
+    internal class HoaDonBan
+    {
+        public string MaHDB { get; set; }
+        public string MaNV { get; set; }
+        public string MaBan { get; set; }
+        public DateTime? NgayBan { get; set; }
+        public string MaKH { get; set; }
+        public decimal? TriGia { get; set; }
+    }
+}
